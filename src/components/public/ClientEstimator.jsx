721500d59dffb05d22cba2678220bd255fdc90dd
A M Pabel <ampabel.edp@gmail.com>
2026-08-31 11:58:37 +0600

    Update Google Apps Script URL to AKfycbzc51p6AZPW5qAngQPvkG05BELOEzNL9EyMU1emhvEbJc3KU4Np_IjYIsUYaz1Vnzw-

--- src/components/public/ClientEstimator.jsx
+++ src/components/public/ClientEstimator.jsx
@@ -361,7 +361,7 @@ export default function ClientEstimator({ isOpen, onClose, initialService = 'gra
     }
 
     // 2. Dispatch POST payload to Google Apps Script Web App Endpoint using mode: "no-cors"
-    const googleWebAppUrl = 'https://script.google.com/macros/s/AKfycby2AtH2DIrVluMCjNGe-TOgnRlrr0APxCJvWmZPjDZc77_5dXXh9V1ROv2fOwckXAP8/exec';
+    const googleWebAppUrl = 'https://script.google.com/macros/s/AKfycbzc51p6AZPW5qAngQPvkG05BELOEzNL9EyMU1emhvEbJc3KU4Np_IjYIsUYaz1Vnzw-/exec';
     
     const googlePayload = {
       client_email: contactInfo || '',
